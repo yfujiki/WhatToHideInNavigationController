ceeb1626b644da3e20973d533f4c8c7e08a0eae3
Yuichi Fujiki <yuichi@chaione.com>
2019-01-10 09:54:27 -0700

    Initial commit.

---
+++ WhatToHideInNavigationController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatToHideInNavigationController
+//
+//  Created by Yuichi Fujiki on 1/10/19.
+//  Copyright © 2019 Yfujiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
